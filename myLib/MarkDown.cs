a41585ade01ed3688a42c052cf802b799f753166
gulssumm <gulsumcetinozlu@gmail.com>
2024-07-23 11:31:47 +0300

    myLib library-MarkDown.cs added

---
+++ myLib/MarkDown.cs
@@ -0,0 +1,12 @@
+﻿using Markdig;
+
+namespace myLib
+{
+    public class MarkDown
+    {
+        public static string ConvertToHtml(string markdown)
+        {
+            return Markdown.ToHtml(markdown);
+        }
+    }
+}
